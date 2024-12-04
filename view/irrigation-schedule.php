5822c69a758b96814f05dcfb44d6b701c8e54cec
Msgasu <edamg123@gmail.com>
2024-12-04 16:27:21 +0000

    added external js

--- view/irrigation-schedule.php
+++ view/irrigation-schedule.php
@@ -141,5 +141,6 @@
 
     <!-- JavaScript (Bootstrap and Dependencies) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/dashboard.js"></script>
 </body>
 </html>
